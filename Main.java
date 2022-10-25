4dc2a4b885efcdb2c751631cb06c28052bb24fed
Abdykojo <116643878+Abdykojo@users.noreply.github.com>
2022-10-25 18:05:34 +0600

    Update Main.java
    second commit

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Person kojo=new Person("Kojo", 21,"Mbank","40000");
         kojo.info();
-
+        System.out.println("Hello");
     }
 }
